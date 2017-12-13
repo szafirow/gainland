4e3c100db332d9c40599802b7d4541e0a7b99a2a
szafirow <p.szafirowicz1@gmail.com>
2017-12-13 16:29:54 +0100

    #Test

---
+++ application/models/Test.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: patryk
+ * Date: 2017-12-13
+ * Time: 16:29
+ */
+
+class Test extends MY_Model
+{
+     public function abc(){
+        echo "dziala";
+    }
+}
