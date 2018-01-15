1c53cb4c7eb263bbe1e2f3e5236643ef7ff519f9
szafirow <p.szafirowicz1@gmail.com>
2018-01-15 21:11:33 +0100

    #Ustawienia poczatek

--- application/views/admin/content/settings.php
+++ application/views/admin/content/settings.php
@@ -1,1 +1,49 @@
-to jest setttings
+<form class="form-horizontal">
+
+
+    <!-- Textarea -->
+    <div class="form-group">
+        <label class="col-md-4 control-label" for="request-description">Logo</label>
+        <div class="col-md-4">
+            <textarea class="form-control" id="request-description" name="request-description"></textarea>
+        </div>
+    </div>
+
+
+    <!-- Text input-->
+    <div class="form-group">
+        <label class="col-md-4 control-label" for="service-request">Nazwa strony</label>
+        <div class="col-md-4">
+            <input id="service-request" name="service-request" type="text" placeholder="" class="form-control input-md"
+                   required="">
+
+        </div>
+    </div>
+
+    <!-- Textarea -->
+    <div class="form-group">
+        <label class="col-md-4 control-label" for="request-description">Krótki opis</label>
+        <div class="col-md-4">
+            <textarea class="form-control" id="request-description" name="request-description"></textarea>
+        </div>
+    </div>
+
+    <!-- Textarea -->
+    <div class="form-group">
+        <label class="col-md-4 control-label" for="request-description">Słowa kluczowe (po przecinku)</label>
+        <div class="col-md-4">
+            <textarea class="form-control" id="request-description" name="request-description"></textarea>
+        </div>
+    </div>
+
+
+    <!-- Button -->
+    <div class="form-group">
+        <label class="col-md-4 control-label"></label>
+        <div class="col-md-4">
+            <button id="singlebutton" name="singlebutton" class="btn btn-primary">Send</button>
+        </div>
+    </div>
+
+
+</form>
