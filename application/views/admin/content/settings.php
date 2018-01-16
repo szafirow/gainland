07d19b01a1d1967918cde1f278edbcf253f6dce2
szafirow <p.szafirowicz1@gmail.com>
2018-01-16 20:19:24 +0100

    #Ustawienia poczatek -view

--- application/views/admin/content/settings.php
+++ application/views/admin/content/settings.php
@@ -1,47 +1,49 @@
 <form class="form-horizontal">
 
 
-    <!-- Textarea -->
     <div class="form-group">
-        <label class="col-md-4 control-label" for="request-description">Logo</label>
-        <div class="col-md-4">
-            <textarea class="form-control" id="request-description" name="request-description"></textarea>
+        <label class="col-md-1 control-label">Logo: </label>
+        <!--<span>Możesz uploadować obrazek wielkości max 250x250px.</span>
+-->
+        <div class="col-md-11">
+            <input id="upload-image" name="logo" type="file" class="file-loading"
+                   data-upload-url="#" accept="image/*">
+            <input name="max_file_size" type="hidden" value="1048576"/>
         </div>
     </div>
 
 
     <!-- Text input-->
     <div class="form-group">
-        <label class="col-md-4 control-label" for="service-request">Nazwa strony</label>
-        <div class="col-md-4">
-            <input id="service-request" name="service-request" type="text" placeholder="" class="form-control input-md"
-                   required="">
-
+        <label class="col-md-1 control-label" for="title">Nazwa strony</label>
+        <div class="col-md-11">
+            <input id="title" name="title" type="text" placeholder="" class="form-control" required="">
         </div>
     </div>
 
     <!-- Textarea -->
     <div class="form-group">
-        <label class="col-md-4 control-label" for="request-description">Krótki opis</label>
-        <div class="col-md-4">
-            <textarea class="form-control" id="request-description" name="request-description"></textarea>
+        <label class="col-md-1 control-label" for="description">Krótki opis</label>
+        <div class="col-md-11">
+            <textarea class="form-control" id="description" rows="5" name="description"></textarea>
         </div>
     </div>
 
     <!-- Textarea -->
     <div class="form-group">
-        <label class="col-md-4 control-label" for="request-description">Słowa kluczowe (po przecinku)</label>
-        <div class="col-md-4">
-            <textarea class="form-control" id="request-description" name="request-description"></textarea>
+        <label class="col-md-1 control-label" for="tags">Słowa kluczowe (po przecinku)</label>
+        <div class="col-md-11">
+            <textarea class="form-control" id="tags" rows="5" name="tags"></textarea>
         </div>
     </div>
 
 
     <!-- Button -->
     <div class="form-group">
-        <label class="col-md-4 control-label"></label>
-        <div class="col-md-4">
-            <button id="singlebutton" name="singlebutton" class="btn btn-primary">Send</button>
+        <label class="col-md-1 control-label"></label>
+        <div class="col-md-11">
+            <input type="hidden" name="action" value="1"/>
+            <input type="submit" class="btn btn-primary btn-block" value="ZAPISZ">
         </div>
     </div>
 
--- application/views/admin/footer.php
+++ application/views/admin/footer.php
@@ -14,5 +14,38 @@
 <script src="<?php echo base_url(); ?>assets/js/jquery.min.js"></script>
 <script src="<?php echo base_url(); ?>assets/js/bootstrap.min.js"></script>
 <script src="<?php echo base_url(); ?>assets/js/admin.js"></script>
+<script src="<?php echo base_url(); ?>assets/js/fileinput.min.js"></script>
+<script>
+    $(document).ready(function () {
+
+        $("#upload-image").fileinput({
+            autoReplace: true,
+            maxImageWidth: 280,
+            maxImageHeight: 280,
+            maxFileCount: 1,
+            allowedFileExtensions: ["jpeg", "jpg", "png", "gif"],
+            showUpload: false,
+            browseOnZoneClick: true,
+            dropZoneTitle: 'Przeciągnij i upuść zdjęcie ',
+            dropZoneClickTitle: 'lub kliknij i wybierz plik.',
+            layoutTemplates:
+                {
+                    actions: '<div class="file-actions">\n' +
+                    '    <div class="file-footer-buttons">\n' +
+                    '       {delete} {zoom} ' +
+                    '    </div>\n' +
+                    '    {drag}\n' +
+                    '    <div class="file-upload-indicator" title="{indicatorTitle}">{indicator}</div>\n' +
+                    '    <div class="clearfix"></div>\n' +
+                    '</div>',
+                    btnDefault: '<button type="{type}" tabindex="500" title="{title}" class="{css}"{status}>{icon}{label}</button>',
+                    btnLink: '<a href="{href}" tabindex="500" title="{title}" class="{css}"{status}>{icon}{label}</a>',
+                    btnBrowse: '<div tabindex="500" class="{css}"{status}>{icon}{label}</div>',
+                }
+        });
+
+    });
+
+</script>
 </body>
 </html>
